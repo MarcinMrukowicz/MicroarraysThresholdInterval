03a71fd4e457c7a43ad52c65936aa3540d660d2b
Karol Lech <karollech787@gmail.com>
2023-03-14 08:14:38 +0100

    Fixed scoring and added comments

--- algorithm.py
+++ algorithm.py
@@ -15,26 +15,30 @@ class Algorithm(BaseEstimator, ClassifierMixin):
 
     def fit(self, X, y=None):
         self.__set_classifiers()
-        X_splitted = self.__split_dataset(X)
+        X_splitted = self.__split_dataset(X)  # podział na podzbiory
 
         for s_index in range(self.s):
             for k_index in range(len(self.k)):
-                self.classifiers[s_index][k_index].fit(X_splitted[s_index], y)
+                self.classifiers[s_index][k_index].fit(X_splitted[s_index],
+                                                       y)  # uczenie klasyfikatora dla poszczególnego podzbioru i k
 
         return self
 
-    def predict(self, X):
-        X_splitted = self.__split_dataset(X)
-        intervals = np.empty((len(X), self.s, 2))
-        for s_index in range(self.s):
-            predictions = np.empty((len(X), len(self.k)))
-            for k_index in range(len(self.k)):
-                classifier = self.classifiers[s_index][k_index]
-                prediction = classifier.predict(X_splitted[s_index])
-                predictions[:, k_index] = prediction
-            interval = np.column_stack((predictions.min(axis=1), predictions.max(axis=1)))
-            intervals[:, s_index] = interval
-        predictions = self.__get_predictions(intervals)
+    def predict_proba(self, X):
+        X_splitted = self.__split_dataset(X)  # podział na podzbiory
+        intervals = np.empty((len(X), self.s,
+                              2))  # inicjalizacja tablicy na przedziały wymiary (ilość obiektów w zbiorze / s / 2 - [down, up])
+        for s_index in range(self.s):  # dla każdego s
+            predictions = np.empty(
+                (len(X), len(self.k)))  # inicjalizacja tablicy na predykcje dla każdego elementu z podzbioru s
+            for k_index in range(len(self.k)):  # dla każdego k
+                classifier = self.classifiers[s_index][k_index]  # pobranie klasyfikatora dla konkretnego podzbioru i k
+                prediction = classifier.predict_proba(X_splitted[s_index])[:, 1]  # predykcja dla konkretnego przedziału
+                predictions[:, k_index] = prediction  # zapisanie w tablicy predykcji
+            interval = np.column_stack((predictions.min(axis=1), predictions.max(
+                axis=1)))  # wybór minimalnej i maksymalnej wartości (przedziału) dla każdego elementu
+            intervals[:, s_index] = interval  # zapis przedziału
+        predictions = self.__get_predictions(intervals)  # agregacja i określenie klasy
 
         return predictions
 
@@ -51,33 +55,34 @@ class Algorithm(BaseEstimator, ClassifierMixin):
         self.classifiers = [
             [KNeighborsClassifier(n_neighbors=self.k[k_index], n_jobs=self.n_jobs) for k_index in range(len(self.k))]
             for _ in range(self.s)
-        ]
+        ]  # tablica klasyfikatorów knn o wymiarach s i długości tablicy k
 
     def __split_dataset(self, X):
-        return np.array_split(X, self.s, axis=1)
+        return np.array_split(X, self.s, axis=1)  # podział na s podzbiorów
 
     def __get_predictions(self, intervals):
         result = []
-        for interval in intervals:
-            aggregation = self.aggregation(interval)
-            if  aggregation[0] > self.t:
-                result.append(1)
-            elif aggregation[0] < self.t:
-                result.append(0)
+        for interval in intervals:  # interval - lista s przedziałów [down(u), up(u)] dla obiektu u
+            aggregation = self.aggregation(interval)  # agregacja przedziałów, wynik [down(u), up(u)]
+            if aggregation[0] > self.t:  # down(u) > t
+                result.append(1)  # main class
+            elif aggregation[1] < self.t:  # up(u) < t
+                result.append(0)  # subordinate class
             else:
-                result.append(None)
+                result.append(None)  # not belong to any class
         return result
 
     def __score_auc(self, y, predictions):
         predictions_array = np.array(predictions)
-        filter_indices = np.where(predictions_array != None)
-        y_filtered = y[filter_indices].tolist()
-        predictions_filtered = predictions_array[filter_indices].tolist()
-        fpr, tpr, thresholds = metrics.roc_curve(y_filtered, predictions_filtered)
-        return metrics.auc(fpr, tpr)
+        filter_indices = np.where(
+            predictions_array != None)  # wyszukanie indeksów gdzie nie ma obiektów niesklasyfikowanych
+        y_filtered = y[filter_indices].tolist()  # filtrowanie rzeczywistych klas po wyszukanych indeksach
+        predictions_filtered = predictions_array[
+            filter_indices].tolist()  # filtrowanie predykcji po wyszukanych indeksach
+        return metrics.roc_auc_score(y_filtered, predictions_filtered)
 
     def __score_coverage(self, predictions):
         return 1 - self.__score_u_area(predictions)
 
     def __score_u_area(self, predictions):
-        return predictions.count(None) / len(predictions)
+        return predictions.count(None) / len(predictions)  # liczba niesklasyfikowanych / liczba wszystkich obiektów
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
 from sklearn.feature_selection import RFE
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import GridSearchCV, cross_val_score
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import RobustScaler
 from sklearn.svm import SVC
@@ -16,12 +16,11 @@ if __name__ == '__main__':
     steps = []
     steps.append(('scaler', RobustScaler()))
     steps.append(('rfe', RFE(estimator=SVC(kernel='linear', random_state=seed), step=1,
-                             n_features_to_select=0.1)))
+                             n_features_to_select=0.1))) # wybranie 10% cech
     steps.append(('algorithm', Algorithm(s=10, k=[3, 5, 7], t=0.5, aggregation=A1, random_state=seed, n_jobs=-1)))
 
     pipe = Pipeline(steps)
-    param_grid = {}
 
-    search = GridSearchCV(pipe, param_grid, n_jobs=-1)
-    search.fit(dataset.X, dataset.y)
-    print(search.best_score_)
+    score = cross_val_score(estimator=pipe, X=dataset.X, y=dataset.y, cv=5, scoring="roc_auc")
+
+    print(score, np.mean(score))
